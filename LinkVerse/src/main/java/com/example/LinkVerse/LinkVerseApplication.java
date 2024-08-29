72e555d4c8ee17a239189f99f7d61407ef49e8bf
Duongne <dangngocduong@dtu.edu.vn>
2024-08-29 23:40:05 +0700

    File

---
+++ LinkVerse/src/main/java/com/example/LinkVerse/LinkVerseApplication.java
@@ -0,0 +1,13 @@
+package com.example.LinkVerse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkVerseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkVerseApplication.class, args);
+	}
+
+}
---
+++ LinkVerse/src/test/java/com/example/LinkVerse/LinkVerseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LinkVerse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkVerseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
