31b2817551c2f25fa8d8bde7c450ed5a099f66aa
Duongne <dangngocduong@dtu.edu.vn>
2024-09-15 23:32:38 +0700

    fix chặn đẩy code,, xoá api

---
+++ LinkVerse/notification-service/src/main/java/com/Linkverse/profile/Service/EmailService.java
@@ -0,0 +1,43 @@
+package com.Linkverse.profile.Service;
+
+
+import com.Linkverse.profile.dto.request.EmailRequest;
+import com.Linkverse.profile.dto.request.SendEmailRequest;
+import com.Linkverse.profile.dto.request.Sender;
+import com.Linkverse.profile.dto.response.EmailResponse;
+import com.Linkverse.profile.exception.AppException;
+import com.Linkverse.profile.exception.ErrorCode;
+import com.Linkverse.profile.repository.httpclient.EmailClient;
+import feign.FeignException;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class EmailService {
+    EmailClient emailClient;
+
+    String apiKey = "your-brevo-apikey";
+
+    public EmailResponse sendEmail(SendEmailRequest request) {
+        EmailRequest emailRequest = EmailRequest.builder()
+                .sender(Sender.builder()
+                        .name("Devteria DotCom")
+                        .email("devteriadotcom@gmail.com")
+                        .build())
+                .to(List.of(request.getTo()))
+                .subject(request.getSubject())
+                .htmlContent(request.getHtmlContent())
+                .build();
+        try {
+            return emailClient.sendEmail(apiKey, emailRequest);
+        } catch (FeignException e){
+            throw new AppException(ErrorCode.CANNOT_SEND_EMAIL);
+        }
+    }
+}
