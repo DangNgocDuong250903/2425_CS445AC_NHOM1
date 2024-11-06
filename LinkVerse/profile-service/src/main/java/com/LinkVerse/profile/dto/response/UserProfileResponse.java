f550a08790bf09def28fd147a62070959b6fabc4
Duongne <dangngocduong@dtu.edu.vn>
2024-11-06 20:47:30 +0700

    Commit full fix all - notification - mail - add fix pass fg pass - send token mail

--- LinkVerse/profile-service/src/main/java/com/LinkVerse/profile/dto/response/UserProfileResponse.java
+++ LinkVerse/profile-service/src/main/java/com/LinkVerse/profile/dto/response/UserProfileResponse.java
@@ -1,9 +1,9 @@
 package com.LinkVerse.profile.dto.response;
 
+import java.time.LocalDate;
+
 import lombok.*;
 import lombok.experimental.FieldDefaults;
-
-import java.time.LocalDate;
 
 @Data
 @Builder
@@ -12,6 +12,9 @@ import java.time.LocalDate;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class UserProfileResponse {
     String id;
+    String username;
+    String status;
+    String email;
     String firstName;
     String lastName;
     LocalDate dob;
