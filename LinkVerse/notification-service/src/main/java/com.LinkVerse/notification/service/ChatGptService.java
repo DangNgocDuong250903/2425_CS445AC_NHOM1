5dd7bce4e0c05f51d97f73d9873a00284a33c982
Duongne <dangngocduong@dtu.edu.vn>
2024-11-12 23:42:40 +0700

    chatno

--- LinkVerse/notification-service/src/main/java/com.LinkVerse/notification/service/ChatGptService.java
+++ LinkVerse/notification-service/src/main/java/com.LinkVerse/notification/service/ChatGptService.java
@@ -1,6 +1,9 @@
 package com.LinkVerse.notification.service;
 
-import org.springframework.http.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
@@ -11,7 +14,9 @@ public class ChatGptService {
 
     private static final String OPENAI_API_KEY = "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";
     private static final String OPENAI_API_URL = "https://api.openai.com/v1/chat/completions";
+
     //Key het han
+    //stop
     public String getChatGptResponse(String prompt) {
         // Lấy thông tin người dùng hiện tại từ SecurityContext
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -26,11 +31,11 @@ public class ChatGptService {
 
         // Cấu trúc body yêu cầu
         String body = "{\n" +
-              "  \"model\": \"gpt-3.5-turbo\", \n" +  // Thay "gpt-4" thành "gpt-3.5-turbo"
-              "  \"prompt\": \"" + prompt + "\",\n" +
-              "  \"max_tokens\": 150,\n" +
-              "  \"temperature\": 0.7\n" +
-              "}";
+                "  \"model\": \"gpt-3.5-turbo\", \n" +  // Thay "gpt-4" thành "gpt-3.5-turbo"
+                "  \"prompt\": \"" + prompt + "\",\n" +
+                "  \"max_tokens\": 150,\n" +
+                "  \"temperature\": 0.7\n" +
+                "}";
 
         // Tạo header với API Key
         HttpHeaders headers = new HttpHeaders();
