6f260c59a12a26349aed3b737258eaba08f68583
Duongne <dangngocduong@dtu.edu.vn>
2024-12-02 20:43:40 +0700

    commit

---
+++ LinkVerse/post-service/target/generated-sources/annotations/com/LinkVerse/post/Mapper/CommentMapperImpl.java
@@ -0,0 +1,32 @@
+package com.LinkVerse.post.Mapper;
+
+import com.LinkVerse.post.dto.response.CommentResponse;
+import com.LinkVerse.post.entity.Comment;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 22.0.2 (Oracle Corporation)"
+)
+@Component
+public class CommentMapperImpl implements CommentMapper {
+
+    @Override
+    public CommentResponse toCommentResponse(Comment comment) {
+        if ( comment == null ) {
+            return null;
+        }
+
+        CommentResponse.CommentResponseBuilder commentResponse = CommentResponse.builder();
+
+        commentResponse.id( comment.getId() );
+        commentResponse.userId( comment.getUserId() );
+        commentResponse.content( comment.getContent() );
+        commentResponse.createdDate( comment.getCreatedDate() );
+        commentResponse.like( comment.getLike() );
+        commentResponse.unlike( comment.getUnlike() );
+
+        return commentResponse.build();
+    }
+}
